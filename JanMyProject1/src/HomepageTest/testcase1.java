5fcd726954117c27cbd6b5bd9168f8fc6123fee0
Prasad <Prasad@DNV>
2021-01-17 12:58:30 +0530

    My First Commit

---
+++ JanMyProject1/src/HomepageTest/testcase1.java
@@ -0,0 +1,14 @@
+package HomepageTest;
+
+public class testcase1 {
+
+	public void m1()
+	{
+		System.out.println("Testcase1 Class >> TestMethod 1");
+	}
+	
+	public void m2()
+	{
+		System.out.println("Testcase1 Class >> TestMethod 2");
+	}
+}
---
+++ JanMyProject1/src/LoginpageTest/testcase2.java
@@ -0,0 +1,9 @@
+package LoginpageTest;
+
+public class testcase2 {
+
+	public void m3()
+	{
+		System.out.println("TestCase 2 Class >> Test Method 3");
+	}
+}
